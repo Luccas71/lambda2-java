0f6fdc9d6bf0540c8ca60efebc4689ba1fb8e20a
Lucas Volpato <lmaciel916@gmail.com>
2024-03-15 11:09:48 -0300

    interface implementation

---
+++ src/app/Main.java
@@ -0,0 +1,29 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entities.Product;
+import util.PredicateProduct;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Notebook", 1200.0));
+        list.add(new Product("Mouse", 90.0));
+        list.add(new Product("Mousepad", 45.0));
+        list.add(new Product("Monitor", 400.0));
+
+        list.removeIf(new PredicateProduct());
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+    }
+}
